9c014a629ecd53c04345acea5a4b5766740236e6
Abhishek <abhishek421486@gmail.com>
2025-01-13 00:22:22 +0530

    added print statement

--- AlarmClock.py
+++ AlarmClock.py
@@ -7,10 +7,12 @@ TIME_FORMAT = "%I:%M:%S %p"
 
 # Function to get alarm time from user
 
+
 def get_alarm():
 
     while True:
-        alarm_time = input( "Enter the time HH:MM:SS AM/PM (two digit format): ")
+        alarm_time = input(
+            "Enter the time HH:MM:SS AM/PM (two digit format): ")
 
         try:
             datetime.datetime.strptime(alarm_time, TIME_FORMAT)
@@ -20,6 +22,8 @@ def get_alarm():
             print("Invalid time format! Please enter time in HH:MM:SS AM/PM format")
 
 # function to play alarm tone
+
+
 def play_sound(sound_file):
     try:
         # play the sound
@@ -27,30 +31,31 @@ def play_sound(sound_file):
     except Exception as e:
         print(f"Error playing sound:{e}")
 
-def alarm_clock(alarm_time, sound_file,repeat_interval: int = 5):
-    
-    date=datetime.datetime.now()
-    date=date.strftime("%Y-%m-%d")
-    
+
+def alarm_clock(alarm_time, sound_file, repeat_interval: int = 5):
+
+    date = datetime.datetime.now()
+    date = date.strftime("%Y-%m-%d")
+
     print(f"Alarm is set for: {alarm_time} at date {date}.\nWaiting...")
     try:
         while True:
             current_time = datetime.datetime.now().strftime("%I:%M:%S %p")
             print(f"Current time is: {current_time}", end="\r")
             if current_time.casefold() == alarm_time.casefold():
-                print("\nIt's time to wake up!")
+                print("\n\nIt's time to wake up!")
                 play_sound(sound_file)
-                
+                print("\nTo stop alarm press ctrl+c")
+
                 # Repeat the alarm at regular intervals
                 for _ in range(repeat_interval-1):
                     time.sleep(4)  # wait for 1 seconds
                     play_sound(sound_file)
                 break
-            
+
             time.sleep(1)
     except KeyboardInterrupt:
         print("\nAlarm stopped.")
-
 
 
 # Function to dipalay the sound list and get user choice
@@ -64,15 +69,15 @@ def display_sounds(sounds):
             input("Choose a sound by entering the corresponding number: "))
         if 1 <= choice <= len(sounds):
             return sounds[choice - 1]
-        print(f"Invalid choice. Please select a number between 1 and {len(sounds)}.")
+        print(f"Invalid choice. Please select a number between 1 and { len(sounds)}.")
 
 
 if __name__ == "__main__":
-    
+
     sounds = Alarm_sounds.sound_file
     print("Welcome to Alarm Clock")
     alarm_time = get_alarm()
     selected_sound = display_sounds(sounds)
-    repeat_interval = int(input("Enter the repeat interval (default is 5): ") or 5)
+    repeat_interval = int(
+        input("Enter the repeat interval (default is 5): ") or 5)
     alarm_clock(alarm_time, selected_sound, repeat_interval)
-
